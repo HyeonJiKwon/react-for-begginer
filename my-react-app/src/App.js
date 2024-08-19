47a0bcd0dc5f4cd07a3bd055b244c0c8e7a79bd7
rnjsguswl39 <rnjsguswl39@gmail.com>
2024-08-20 00:03:43 +0900

    chapter::5.1 Tour of CRA

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css"; 
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1 className={styles.title}>Welcome</h1>
+      <Button text={'hi'}></Button>
+      <Button></Button>
     </div>
   );
 }
