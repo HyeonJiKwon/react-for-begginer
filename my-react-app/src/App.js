78e99a60aee55d6b8155899e518c76486a4e794d
rnjsguswl39 <rnjsguswl39@gmail.com>
2024-08-20 20:39:51 +0900

    topic::6.4 Cleanup

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,34 +2,23 @@ import Button from "./Button";
 import styles from "./App.module.css"; 
 import { useEffect, useState } from "react";
 
+function Hello() {
+  useEffect(()=>{
+    console.log('effectFn: creat')
+    // 컴포넌트가 destroy되는 시점에 실행됨
+    return ()=>console.log('effectFn: destroy');
+  },[]);
+  return (
+    <h1>hello</h1>
+  );
+}
 function App() {
-  const [value , setValue] = useState(0)
-  const [keyword , setKeyword] = useState("")
-  const onChange = (e) => setKeyword(e.target.value)
-  // console.log('all time');
-  const onlyOnce = () => {
-    console.log('onlyOnce');
-  }
-  useEffect(onlyOnce, [])
-  useEffect(()=>{
-    console.log('run when change keyword')
-    if(keyword && keyword.length > 5){
-      console.log('keyword ',keyword)
-    }
-  }, [keyword]);
-
-  useEffect(()=>{
-    console.log('run when change value')
-  }, [value]);
-
-  useEffect(()=>{
-    console.log('run when change both')
-  }, [keyword,value]);
+  const [showing , setShowing] = useState(false)
+  
   return (
     <div>
-      <input onChange={onChange} value={keyword} type="text"/>
-      <h1 className={styles.title}>{value}</h1>
-      <button onClick={()=>setValue((p)=>p+1)}>set</button>
+      {showing ? <Hello></Hello> : null}
+      <button onClick={()=>setShowing((p)=>!p)}>{showing ? 'hide' :'show'}</button>
     </div>
   );
 }
